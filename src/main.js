f75ef2c2e5098d9f03c1d869312b8dd3bca84fc0
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2022-07-01 16:50:28 +0200

    fix rewriting url also for standard path

--- src/main.js
+++ src/main.js
@@ -24,7 +24,8 @@ function toVhost(item, id, host, template) {
 }
 
 function toApacheProxyPass(item, port = 8889, triplestore) {
-  temp = triplestore === 'graphdb' ? `RewriteRule ^/${item[0]}(.*)$ http://%{SERVER_NAME}/resource?uri=http://data.odeuropa.eu/${item[0]}$1` : ''
+  to_conv = triplestore === 'graphdb' && !DEFAULTS[triplestore].includes(item[0])
+  temp = to_conv ? `RewriteRule ^/${item[0]}(.*)$ http://%{SERVER_NAME}/resource?uri=http://data.odeuropa.eu/${item[0]}$1` : ''
   return `    ${temp}
       ProxyPass /${item[0]} http://localhost:${port}/${item[1]}
     ProxyPassReverse /${item[0]} http://localhost:${port}/${item[1]}
